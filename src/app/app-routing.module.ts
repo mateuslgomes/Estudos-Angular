0dd90c9837fdf1ddf56c0f4bf96793ba66784c7b
mateuslgomes <dev.mateuslgomes@gmail.com>
2023-08-08 16:59:30 -0300

    Criando modulos, add rotas e redirecionamentos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { NovoComponent } from './componentes/novo/novo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'novo', component: NovoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
+import { NovoComponent } from './componentes/novo/novo.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NovoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
